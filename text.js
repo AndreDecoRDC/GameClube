9ff89844fb7f0563b3fe9eb6d637128e82146a47
BrunFrade <bfribsv@gmail.com>
2024-08-29 18:28:32 -0300

    branch temporario de correcao

---
+++ text.js
@@ -0,0 +1,39 @@
+const andre_txt = document.getElementById("andre-text")
+const fred_txt = document.getElementById("fred-text")
+const bruno_txt = document.getElementById("bruno-text")
+
+function showFadeInAndre(){
+
+    andre_txt.style.animation = "1s fade-in forwards"
+
+}
+
+function showFadeOutAndre(){
+
+    andre_txt.style.animation = "1s fade-out forwards"
+
+}
+
+function showFadeInFred(){
+
+    fred_txt.style.animation = "1s fade-in forwards"
+
+}
+
+function showFadeOutFred(){
+
+    fred_txt.style.animation = "1s fade-out forwards"
+
+}
+
+function showFadeInBruno(){
+
+    bruno_txt.style.animation = "1s fade-in forwards"
+
+}
+
+function showFadeOutBruno(){
+
+    bruno_txt.style.animation = "1s fade-out forwards"
+
+}
